4d411c883c15e287ddea833fcfaed12c1d80c098
Keiju E <keiju.eason@yahoo.com>
2025-02-25 11:08:15 -0500

    Created PSQL for users and started express.

--- server/server.js
+++ server/server.js
@@ -1,20 +1,41 @@
 // dependencies
+require("dotenv").config();
+const bcrypt = require('bcrypt');
 const express = require("express");
-const cors = require("cors")
 
-//use cors to communicate with the frontend
+// talk to the database 
+const db = require("./db");
+
+// create an express instance
 const app = express();
-app.use(cors()); 
 app.use(express.json());
 
-const PORT = 3701;
-
-//TODO: Have postgres actually set up
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+// create an account (NEED TO ADD PASSWORD ENCRYPTION)
+app.post("/signup", async (req, res) => {
+    const {username, password, email} = req.body;
+    //console.log(req.body); //uncomment to see what is being passed in 
+    try{
+        const results = db.query(
+            'INSERT INTO users (username, password, email) VALUES ($1, $2, $3)', [username, password, email]
+        );
+        res.status(201).send({ message: 'Signup succesful'});
+    } catch (error) {
+        res.status(500).send({ error: "Signup failed"});
+    }
 });
 
+// login to account (incomplete)
+app.get("/login", async (req, res) => {
+    res.json({
+        status: "sucess",
+        user: "test user",
+    })
+});
+
+// set port using .env; if no value is defined, set to 3000
+const PORT = process.env.PORT || 3000;
+
+// running 
 app.listen(PORT, () =>{
     console.log(`Server listening on port ${PORT}`);
-})
+});
