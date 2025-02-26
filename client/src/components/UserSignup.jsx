a429306c7e649b2c803e02fb107b221800f0af39
HuyNguyen17 <genericnguyenhuy17@gmail.com>
2025-02-26 17:49:44 -0500

    Create user signup component on homepage

---
+++ client/src/components/UserSignup.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import api from "../api/api"
+const UserSignup = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    const Signup = async (e) => {
+        try {
+            e.preventDefault(); //don't refresh
+            const body =
+                {
+                    username,
+                    password,
+                    email
+                }
+            api.post("/signup", body)
+                .then(function (response) {
+                console.log(response);
+                })
+                .catch(function (err) {
+                console.log(err);
+                });
+        }
+        catch(err) {
+            console.error(err.message);
+        }
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={Signup}>
+                <label for="email">Email</label><br/>
+                <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)}></input><br/>
+                <label for="username">Username</label><br/>
+                <input type="text" id="username" name="username" value={username} onChange={e => setUsername(e.target.value)}></input><br/>
+                <label for="password">Password</label><br/>
+                <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)}></input><br/>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default UserSignup;
