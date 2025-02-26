7b2ec101ca75d7774cf9554942628118b8deb2b5
HuyNguyen17 <genericnguyenhuy17@gmail.com>
2025-02-26 16:46:45 -0500

    add linter configuration

---
+++ client/.eslint.config.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {
+        "plugins": [
+            "react"
+        ],
+        "rules": {
+            "react/jsx-uses-vars": "error",
+            "react/jsx-uses-react": "error"
+        }
+    }
+];
