b1194740bf5b1269043a17321ba26b24e1779312
Keiju E <keiju.eason@yahoo.com>
2025-02-26 21:27:19 -0500

    Updated api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 //TODO: change the port to be dynamic based on env
 export default axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "http://localhost:3001"
 })
