2715d48998500e742278531f0bd6d66bddedbbf4
AndrewZelitt <azelitt@ufl.edu>
2025-03-10 13:06:14 -0400

    going to work on the projects route

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -5,4 +5,57 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const authorization = require("../authorization");
 
+
+router.post('/create_project', async (req, res) => {
+    const{project_name, username,project_description} = req.body;
+        if(!project_name){
+            return res.status(400).send({ error: "Project needs to have a name."});
+
+        }
+        try{
+            await db.query(
+                'INSERT INTO projects (projectname) VALUES($1)' , [project_name] 
+            );
+            res.status(201).send({ message: 'Project created'});
+        }
+        catch(error){
+            res.status(500).send({ error: "Project Name Taken or null or too long"});
+        }
+        //get the ID of the project and link it to the user.
+        try{
+            const results = await db.query(
+                        'SELECT * FROM users WHERE username = $1',
+                        [username]
+                    );
+            // if username does not exist, throw error
+            if (results.rowCount === 0) {
+                return res.status(401).send( { error: 'Nonexistant User'});
+            }
+            const user = results.rows[0];
+
+            const projres = await db.query(
+                'SELECT * FROM projects WHERE projectname = $1',
+                [project_name]
+            );
+
+            // if project name does not exist, throw error
+            if (results.rowCount === 0) {
+                return res.status(401).send( { error: 'Nonexistant project'});
+             }
+            const project = projres.rows[0];
+
+            await db.query(
+                'INSERT INTO link_projects (user_id,project_id) VALUES($1,$2)' , [user.id ,project.id] 
+            );
+            if(description.length > 5000){
+                return res.status(401).send( { error: 'Description too Long'});
+            }
+            await db.query(
+                'UPDATE projects SET description = $1 WHERE id = $2', [project_description, project.id]
+            );
+
+        }catch(error){
+            res.status(500).send({ error: "Project or Username doesnt exist."});
+        }
+    });
 module.exports = router;
