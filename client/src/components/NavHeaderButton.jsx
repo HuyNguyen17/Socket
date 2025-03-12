502ce668c4bf70a9d04ce81cf50abeed4e424b1b
HuyNguyen17 <genericnguyenhuy17@gmail.com>
2025-03-11 21:27:01 -0400

    wrap button in a link so we can do normal link stuff with it

--- client/src/components/NavHeaderButton.jsx
+++ client/src/components/NavHeaderButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const NavHeaderButton = ({target, label}) => {
@@ -8,8 +8,8 @@ const NavHeaderButton = ({target, label}) => {
     const navigate = useNavigate();
 
     return(
+        <Link to={target}>
         <Button
-        onClick={() => navigate(target)}
         style={{
             padding: '10px 20px', fontSize: '16px', backgroundColor: 'transparent', color: 'black', border: 'none',
             borderBottom: '2px solid transparent', cursor: 'pointer', marginLeft: '10px', transition: 'all 0.3s ease'
@@ -19,6 +19,7 @@ const NavHeaderButton = ({target, label}) => {
         >
         {label}
         </Button>
+        </Link>
     )
 
 };
