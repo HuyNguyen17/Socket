452ccf0fe6e308e5cdb27916523fc4202dbefb71
HuyNguyen17 <genericnguyenhuy17@gmail.com>
2025-03-11 21:49:08 -0400

    consolidate dashboard and home pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import SignupPage from "./routes/SignupPage"
 import UsersListPage from "./routes/UsersListPage";
 import ProjectsListPage from "./routes/ProjectsListPage";
 import UserProfilePage from "./routes/UserProfilePage";
-import DashboardPage from "./routes/DashboardPage";
 import SignupResultPage from "./routes/SignupResult";
 
 const App = () => {
@@ -15,13 +14,14 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+
         <Route path="/signup" element={<SignupPage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/logout" element={<LogoutPage/>}/>
         <Route path="/users" element={<UsersListPage/>}/>
         <Route path="/projects" element={<ProjectsListPage/>}/>
         <Route path="/signup-result" element={<SignupResultPage/>}/>
-        <Route path="/home" element={<DashboardPage/>}/>
         <Route path="/users/:username" element={<UserProfilePage />} />
       </Routes>
     </Router>
