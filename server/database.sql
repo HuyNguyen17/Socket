2a0792e9e86625550d36b0105ec3cb3a971b4dfc
jdados <jeremiasdados@ufl.edu>
2025-03-19 14:48:11 -0400

    Front-end only changes to make the websites look nicer.

--- server/database.sql
+++ server/database.sql
@@ -14,6 +14,7 @@
 
 CREATE DATABASE socket;
 
+<<<<<<< HEAD
 CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(60) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -24,6 +25,11 @@ CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY,
     description VARCHAR(5000),
     profile_pic bytea
     );
+=======
+CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY, username VARCHAR(60) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, email VARCHAR(255) UNIQUE,
+	linkedin VARCHAR(255), major VARCHAR(255), year VARCHAR(20), description VARCHAR(5000), profile_pic bytea
+);
+>>>>>>> 849414f (Front-end only changes to make the websites look nicer.)
 
 CREATE TABLE projects (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
