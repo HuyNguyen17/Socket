7f9908c9054fb014d350a0b1111645808f44b690
Jeremiah Dados <vanyar2045@gmail.com>
2025-03-23 20:46:43 -0400

    Added an option to create a project and save it to backend.

--- server/database.sql
+++ server/database.sql
@@ -23,6 +23,7 @@ CREATE DATABASE socket;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(60) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -46,6 +47,8 @@ CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY, username VARCHAR(60) NOT 
 >>>>>>> 6143ce1 (added posts.js)
 =======
 >>>>>>> 8685617 (Front-end only changes to make the websites look nicer.)
+=======
+>>>>>>> 736f096 (Added an option to create a project and save it to backend.)
 CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY, username VARCHAR(60) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, email VARCHAR(255) UNIQUE, linkedin VARCHAR(255), major VARCHAR(255), year VARCHAR(20), description VARCHAR(5000), profile_pic bytea);
 =======
 =======
@@ -78,6 +81,8 @@ CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY, username VARCHAR(60) NOT 
 >>>>>>> b81b52c (added posts.js)
 =======
 >>>>>>> 2a0792e (Front-end only changes to make the websites look nicer.)
+=======
+>>>>>>> 386ac73 (Added an option to create a project and save it to backend.)
 CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(60) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -116,8 +121,17 @@ CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY, username VARCHAR(60) NOT 
 	linkedin VARCHAR(255), major VARCHAR(255), year VARCHAR(20), description VARCHAR(5000), profile_pic bytea
 );
 >>>>>>> 849414f (Front-end only changes to make the websites look nicer.)
+<<<<<<< HEAD
 >>>>>>> 2a0792e (Front-end only changes to make the websites look nicer.)
+<<<<<<< HEAD
 >>>>>>> 8685617 (Front-end only changes to make the websites look nicer.)
+=======
+=======
+=======
+CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY, username VARCHAR(60) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, email VARCHAR(255) UNIQUE, linkedin VARCHAR(255), major VARCHAR(255), year VARCHAR(20), description VARCHAR(5000), profile_pic bytea);
+>>>>>>> 8642c5f (Added an option to create a project and save it to backend.)
+>>>>>>> 386ac73 (Added an option to create a project and save it to backend.)
+>>>>>>> 736f096 (Added an option to create a project and save it to backend.)
 
 CREATE TABLE projects (id BIGSERIAL NOT NULL PRIMARY KEY, projectname VARCHAR(60) NOT NULL UNIQUE, description VARCHAR(5000), project_pic bytea);
 
