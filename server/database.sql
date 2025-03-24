09b53d07d2dbc0c12be4ff9f5b2fb3ffa4d2fe3c
Jeremiah Dados <vanyar2045@gmail.com>
2025-03-23 20:46:43 -0400

    Added an option to create a project and save it to backend.

--- server/database.sql
+++ server/database.sql
@@ -19,6 +19,7 @@ CREATE DATABASE socket;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY,
     username VARCHAR(60) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -75,6 +76,9 @@ CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY,
 >>>>>>> 86fcde0 (working on orgs)
 =======
 >>>>>>> 9698984 (added posts.js)
+=======
+CREATE TABLE users (id BIGSERIAL NOT NULL PRIMARY KEY, username VARCHAR(60) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, email VARCHAR(255) UNIQUE, linkedin VARCHAR(255), major VARCHAR(255), year VARCHAR(20), description VARCHAR(5000), profile_pic bytea);
+>>>>>>> 8642c5f (Added an option to create a project and save it to backend.)
 
 CREATE TABLE projects (id BIGSERIAL NOT NULL PRIMARY KEY, projectname VARCHAR(60) NOT NULL UNIQUE, description VARCHAR(5000), project_pic bytea);
 
