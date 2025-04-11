31b6d3a376b76e9bee8c0cf71eb18f35c1ca2fee
AndrewZelitt <azelitt@ufl.edu>
2025-04-11 13:06:23 -0400

    editing worked, rebasing annoying

--- client/src/components/EditProfile.jsx
+++ client/src/components/EditProfile.jsx
@@ -1,24 +1,58 @@
-import React, {useState} from "react";
-import {useNavigate} from 'react-router-dom';
+
+import React, {useState, useEffect} from "react";
+import {useNavigate, useParams} from 'react-router-dom';
+
 import api from "../api/api"
 
 const EditProfile = () => {
+    const [userData, setUserData] = useState({linkedin  : "",
+                                                year    : "",
+                                                major   : "",
+                                                description : ""});
+    
     const navigate = useNavigate();
-    const [linkedin, setLinkedIn] = useState("");
-    const [major, setMajor] = useState("");
-    const [year , setYear] = useState("");
-    const [description, setDescription] = useState("");
+
     
     const [editFailed, setFailure] = useState(false);
+    const [error, setError] = useState(false);
+    
+
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            try {
+                console.log("fetching");
+                const token = localStorage.getItem('authToken');
+                if (!token) {
+                    setError(true);
+                    return;
+                }
+
+                const usernameResponse = await api.get(`/users/decode`, {
+                    headers: {
+                        Authorization: `${token}`, // Send the token in the header
+                    },
+                });
+                
+                const username = (usernameResponse.data);
+
+                const response = await api.get(`/users/getuser/${username}`);
+                console.log(response.data);
+                setUserData(response.data);
+                console.log(userData);
+            } catch (error) {
+                console.error("Error fetching user profile:", error);
+                setError(true);
+            }
+        };
+
+        fetchUserProfile();
+    }, []);
 
     const HandleEdit = async (e) => {
         try {
             e.preventDefault(); //don't refresh
             const body = {
-                    linkedin,
-                    major,
-                    year,
-                    description,
+                    ...userData
             }
             await api.put("users/edit", body)
                 .then(function (response) {
@@ -39,18 +73,66 @@ const EditProfile = () => {
 
     }
 
+    const handleLinkedinChange = async (e) => {
+        setUserData({
+            ...userData,
+            linkedin: e.target.value
+        })
+    }
+    const handleMajorChange = async (e) => {
+        setUserData({
+            ...userData,
+            major: e.target.value
+        })
+    }
+    const handleYearChange = async (e) => {
+        setUserData({
+            ...userData,
+            year: e.target.value
+        })
+    }
+    const handleDescriptionChange = async (e) => {
+        setUserData({
+            ...userData,
+            description: e.target.value
+        })
+    }
+
     return (
         <div>
             {editFailed ? <div style={{color: "red"}}>Edit Failed!</div> : ""}
             <form onSubmit={HandleEdit}>
                 <label htmlFor="linkedin">LinkedIn</label><br/>
-                <input type="text" id="linkedin" name="linkedin" value={linkedin} onChange={e => setLinkedIn(e.target.value)}></input><br/>
+                <input
+                    type="text" 
+                    id="linkedin" 
+                    name="linkedin"
+                    value={userData.linkedin}
+                    onChange={handleLinkedinChange}></input><br/>
+
                 <label htmlFor="major">Major</label><br/>
-                <input type="text" id="major" name="major" value={major} onChange={e => setMajor(e.target.value)}></input><br/>
+                <input 
+                    type="text"
+                    id="major"
+                    name="major"
+                    value={userData.major} 
+                    onChange={handleMajorChange}></input><br/>
+                
                 <label htmlFor="year">Year</label><br/>
-                <input type="text" id="year" name="year" value={year} onChange={e => setYear(e.target.value)}></input><br/>
+                <input 
+                type="text"
+                id="year" 
+                name="year" 
+                value={userData.year} 
+                onChange={handleYearChange}></input><br/>
+                
                 <label htmlFor="description">Description</label><br />
-                <textarea id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)}/><br />
+                <textarea
+                    id="description"
+                    name="description"
+                    value={userData.description}
+                    onChange={handleDescriptionChange}/><br />
+
                 <button type="submit" style={{marginTop: '10px', marginBottom: '30px'}}>Submit</button>
             </form>
         </div>
